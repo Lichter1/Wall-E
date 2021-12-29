62b21126e22971adc8e5327cbcaa6fe3c5d320cf
Lichter1 <93467797+Lichter1@users.noreply.github.com>
2021-12-29 08:46:40 +0000

    Update Lerning_Pre-Amble.py

--- Lerning_Pre-Amble.py
+++ Lerning_Pre-Amble.py
@@ -11,19 +11,21 @@ if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0',9600, timeout=1)
     ser.flush()
     while True:
+        
+        
         #### Read joyStick ##
         jsVal = js.getJS()
-
-        ### Map joyStick to angle ###
-        angle = str(int(_map(jsVal['axis3'], 1, -1,0, 180)))
-
-        ### Print angle in the serial ###
-        print (test.encode('utf-8'))
-        ser.write(test.encode('utf-8'))
-
+        
+        if ser.in_waiting > 0:
+            ### Map joyStick to angle ###
+            angle = str(int(_map(jsVal['axis3'], 1, -1,0, 180)))
+            
+            ### Print angle in the serial ###
+            print (test.encode('utf-8'))
+            ser.write(test.encode('utf-8'))
+        
         sleep(0.1)
-
+        
         ### If "F5" on joyStick presst, exit ###
         if jsVal['4'] == 1:
-
             break
