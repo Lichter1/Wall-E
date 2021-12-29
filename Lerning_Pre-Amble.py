c267498793df3ac66688014786710fd95b783bad
Lichter1 <93467797+Lichter1@users.noreply.github.com>
2021-12-29 08:31:45 +0000

    Add files via upload

---
+++ Lerning_Pre-Amble.py
@@ -0,0 +1,29 @@
+import serial
+from time import sleep
+import JoyStickModule as js
+
+
+def _map(x, in_min, in_max, out_min, out_max):
+    return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
+
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM0',9600, timeout=1)
+    ser.flush()
+    while True:
+        #### Read joyStick ##
+        jsVal = js.getJS()
+
+        ### Map joyStick to angle ###
+        angle = str(int(_map(jsVal['axis3'], 1, -1,0, 180)))
+
+        ### Print angle in the serial ###
+        print (test.encode('utf-8'))
+        ser.write(test.encode('utf-8'))
+
+        sleep(0.1)
+
+        ### If "F5" on joyStick presst, exit ###
+        if jsVal['4'] == 1:
+
+            break
